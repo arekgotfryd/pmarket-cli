4b3b49635caf540a4942069460157ae8fa487cfa
arekgotfryd <arekgotfryd@g.pl>
2023-05-29 22:56:27 +0200

    market buy order option added

--- src/main.ts
+++ src/main.ts
@@ -15,17 +15,31 @@ async function bootstrap() {
   program
     .version("1.0.0")
     .description("A CLI to trade on Polymarket")
-    .option("-l, --ls <question filter>", "List available markets with question filter")
+    .option("-l, --ls <question filter>", "List available markets with question filter. Usage: poly-cli -l <question filter>")
+    .option("-b, --buy <args...>", "Market buy token. Usage: poly-cli -b <token id> <amount in dollars> <price>")
     .parse(process.argv);
+
+  //show help if no arguments are passed
+  if (!process.argv.slice(2).length) {
+    program.outputHelp();
+  }
 
 
   const options = program.opts();
   console.log(options);
   //list all markets matching filter
-  if(options.ls) {
+  if (options.ls) {
     console.log(options.ls);
     const markets = await polymarketService.getMarkets();
     console.table(markets.filter(market => market.question.toLowerCase().includes(options.ls.toLowerCase())));
+  }
+  //buy token
+  if (options.buy && options.buy.length == 3) {
+    const token_id = options.buy[0];
+    const amountInDollars = +options.buy[1];
+    const price = +options.buy[2];
+    const order = await polymarketService.marketOrder(token_id, amountInDollars, price);
+    console.log(order);
   }
 }
 bootstrap();
