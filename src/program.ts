8efd666efcc49de95a6e94a91b312c231022bafa
arekgotfryd <arekgotfryd@g.pl>
2023-06-15 10:47:22 +0200

    added: license + readme + publish npm package (#13 #7)

--- src/program.ts
+++ src/program.ts
@@ -4,10 +4,10 @@ export const getProgram = (): Command => {
   const program = new Command();
   program
     .version("0.7.0")
-    .description("A CLI to trade on Polymarket")
+    .description("Command line interface for Polymarket")
     .option("-l, --list <question filter>", "List available markets with question filter. Usage: poly-cli -l <question filter>")
-    .option("-b, --buy <args...>", "Buy token. Usage: poly-cli -b <token id> <amount in USDC> <price>")
-    .option("-s, --sell <args...>", "Sell token. Usage: poly-cli -s <token id> <amount of tokens> <price>")
+    .option("-b, --buy <args...>", "Buy token order. Usage: poly-cli -b <token id> <amount in USDC> <price>")
+    .option("-s, --sell <args...>", "Sell token order. Usage: poly-cli -s <token id> <amount of tokens> <price>")
     .option("-a, --allowance <amount in USDC>", "Set USDC allowance for CTFExchange contract. Usage: poly-cli -a <amount in USDC>")
     .option("-o, --orderBook <tokenId>", "Show order book for specific tokenId. Usage: poly-cli -o <token id>")
     .option("-c, --cancelAll", "Cancel all open orders. Usage: poly-cli -c")
