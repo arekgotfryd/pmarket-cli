da73495859f6ff405bcceab9b6fc4a5056eab2dc
arekgotfryd <arekgotfryd@g.pl>
2023-06-15 22:11:50 +0200

    added webpack config + turn off nest logging

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  mode: 'production',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true, // Preserve class names
+          keep_fnames: true, // Preserve function names
+        },
+      }),
+    ],
+  },
+};
